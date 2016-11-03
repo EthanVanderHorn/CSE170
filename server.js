61f061c845bea36e468e56fde3bdec11c1d282f1
EthanVanderHorn <edvander@ucsd.edu>
2016-11-02 21:58:08 -0700

    fixed some links and routing. Hardcoded in some sample data for the user page.

--- server.js
+++ server.js
@@ -9,9 +9,29 @@ var morgan     = require('morgan'); 		// used to see requests
 var mongoose   = require('mongoose');
 var config 	   = require('./config');
 var path 	   = require('path');
+var http	   = require('http');
+var handlebars = require('express-handlebars')
+
+// Example route
+// var user = require('./routes/user');
+
+var app = express();
 
 // APP CONFIGURATION ==================
 // ====================================
+
+app.set('views', path.join(__dirname, 'public/app/views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded());
+//app.use(express.methodOverride());
+//app.use(express.cookieParser('Intro HCI secret key'));
+//app.use(express.session());
+
+
 // use body parser so we can grab information from POST requests
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -39,8 +59,20 @@ app.use(express.static(__dirname + '/public'));
 // MAIN CATCHALL ROUTE --------------- 
 // SEND USERS TO FRONTEND ------------
 // has to be registered after API ROUTES
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/app/views/splash.html'));
+});
+
+app.get('/user/:username', function(req, res){
+	var username = req.params.username;
+	res.render('index', {
+		'UserName': username, 
+		'TeamName': 'Cogs 120', 
+		'TeamDescription': 'dis the team description yo', 
+		'Member1': 'Connie Guan', 
+		'Member2': 'Jacqui Bontigao', 
+		'Member3': 'Miguel Vargas', 
+		'Member4': 'Ethan Vander Horn'});
 });
 
 // START THE SERVER
