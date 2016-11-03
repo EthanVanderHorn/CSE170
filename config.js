70f965059e421c1ca4a86422792a79111544f0b6
Miguel Vargas <miguelvargas.121@gmail.com>
2016-11-02 19:02:49 -0700

    UPDATE: small files changes

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 module.exports = {
-	'port': process.env.PORT || 8080,
-	// 'database': 'mongodb://miguelvargas:Dotcom12@ds019946.mlab.com:19946/athletemanagerdb',
-	'secret': 'ilovebourbon'
+	'port': process.env.PORT || 8080
 };
