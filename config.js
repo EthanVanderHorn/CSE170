e6d32f0d482c304571912c8ce45aeb51a26aab18
Miguel Vargas <miguelvargas.121@gmail.com>
2016-11-02 18:29:55 -0700

    UPDATE: new design implemented & added splash page

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	// 'database': 'mongodb://miguelvargas:Dotcom12@ds019946.mlab.com:19946/athletemanagerdb',
+	'secret': 'ilovebourbon'
+};
---
+++ public/assets/js/sidemenu.js
@@ -0,0 +1,27 @@
+/*====================================
+=            ON DOM READY            =
+====================================*/
+$(function() {
+  $('.toggle-nav').click(function() {
+    // Calling a function in case you want to expand upon this.
+    toggleNav();
+  });
+});
+
+
+/*========================================
+=            CUSTOM FUNCTIONS            =
+========================================*/
+function toggleNav() {
+	if ($('#site-wrapper').hasClass('show-nav')) {
+	  // Do things on Nav Close
+	  $('#site-wrapper').removeClass('show-nav');
+	} else {
+	  // Do things on Nav Open
+	  $('#site-wrapper').addClass('show-nav');
+	}
+};
+
+$(".btn-fix").mouseup(function(){
+    $(this).blur();
+});
