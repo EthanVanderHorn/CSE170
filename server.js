c05082db86601adc675452fb4da34b18dd1237f5
EthanVanderHorn <edvander@ucsd.edu>
2016-11-17 23:35:30 -0800

    oops now i removed the routing

--- server.js
+++ server.js
@@ -108,30 +108,7 @@ app.get('/user/:username/account-settings', function(req, res){
 	res.render('settings', {'UserName': username});
 });
 
-<<<<<<< HEAD
-=======
-app.get('/user/:username/:group/home2', function(req, res){
-	var username = req.params.username;
-	var group = (req.params.group).toLowerCase();
-	var groupData;
-	var currentElement;
 
-	data['isOriginal'] = false;
-	if(obj === undefined){
-		fs = require('fs');
-		obj = JSON.parse(fs.readFileSync('./mock-data/groups.json', 'utf8').toString());
-	}
-	for (var i = obj.groups.length - 1; i >= 0; i--) {
-		currentElement = obj.groups[i];
-		if(currentElement.TeamURL ==  group){
-			groupData = currentElement;
-			break;
-		}
-	}
-	res.render('index', {"groupData": groupData, "UserName": username, "groups": obj.groups});
-});
-
->>>>>>> 50af97278762cb1b4af05a3b8ec528a1cf335fa0
 app.get('/user/:username/:group', function(req, res){
 	var username = req.params.username;
 	var group = (req.params.group).toLowerCase();
